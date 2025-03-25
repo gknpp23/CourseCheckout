332fc8b38bc8664a0f1335819d0205b3e4c49d70
Gabriel Knupp <gabrielknpp@gmail.com>
2025-03-24 21:34:43 -0300

    feat: add email service with Nodemailer

---
+++ backend/src/services/email.services.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+const sendEmail = (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erro ao enviar e-mail:', error);
+    } else {
+      console.log('E-mail enviado:', info.response);
+    }
+  });
+};
+
+module.exports = { sendEmail };
