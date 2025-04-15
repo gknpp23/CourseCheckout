bbcccecfd8ccad6f1f8a92da9ed26fd7749310fb
Gabriel Knupp <gabrielknpp@gmail.com>
2025-04-15 18:06:56 -0300

    refactor(rotas): componente removido do mapeamento de rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
-import { PixPaymentComponent } from './components/pix-payment/pix-payment.component';
+
 
 export const routes: Routes = [
   { path: '', component: RegistrationFormComponent }, // Rota principal
-  { path: 'pix', component: PixPaymentComponent},
   { path: '**', redirectTo: '' } // Redireciona rotas inválidas
 ];
