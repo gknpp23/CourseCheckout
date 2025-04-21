06d3c785df569aefcc21c2bbae20b8cbe082c40b
Gabriel Knupp <gabrielknpp@gmail.com>
2025-04-21 19:43:19 -0300

    feat(routes): adicionar rota para página de sucesso
    Inclui a rota '/success' que carrega dinamicamente o componente SuccessPageComponent.
    Ajusta a ordem das rotas para garantir que '**' (rota coringa) venha por último, evitando conflitos com rotas válidas.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,18 @@ import { Routes } from '@angular/router';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 
+
 export const routes: Routes = [
   { path: '', component: RegistrationFormComponent }, // Rota principal
-  { path: '**', redirectTo: '' } // Redireciona rotas inválidas
+  {
+    path: 'success',
+    loadComponent: () =>
+      import('./components/success-page/success-page.component').then(
+        (m) => m.SuccessPageComponent
+      ),
+  },
+  //Essa rota deve sempre vir por último
+  { path: '**', redirectTo: '' },// Redireciona rotas inválidas
+  
+  
 ];
